45504785f1cc64ea46804d4e379c4d8475dd4dcc
Peter-cloud-web <petergichia165@gmail.com>
2023-11-28 18:08:20 +0300

    Create domain module

---
+++ data/src/main/java/com/example/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.example.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+//    @Provides
+//    fun getHttpClient(): HttpClient {
+//
+//    }
+}
