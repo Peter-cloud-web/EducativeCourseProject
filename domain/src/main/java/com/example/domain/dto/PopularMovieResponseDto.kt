5e59897c8a31fa59654abeb328ae483f4513f992
Peter-cloud-web <petergichia165@gmail.com>
2023-12-08 09:04:05 +0300

    Add DTO data classes

---
+++ domain/src/main/java/com/example/domain/dto/PopularMovieResponseDto.kt
@@ -0,0 +1,28 @@
+package com.example.domain.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@kotlinx.serialization.Serializable
+data class PopularMovieResponseDto(
+    @SerialName("page") val page: Int,
+    @SerialName("results") val results: List<PopularMovieDto>,
+    @SerialName("total_pages") val pages: Int,
+    @SerialName("total_results") val totalResults: Int
+)
+
+@kotlinx.serialization.Serializable
+data class TopRatedMovieResponseDto(
+    @SerialName("page") val page: Int,
+    @SerialName("results") val movies: List<TopRatedMoviesDto>,
+    @SerialName("total_pages") val pages: Int,
+    @SerialName("total_results") val totalResults: Int
+)
+
+@Serializable
+data class UpComingMovieResponseDto(
+    @SerialName("page") val page: Int,
+    @SerialName("results") val movies: List<UpComingMoviesDto>,
+    @SerialName("total_pages") val pages: Int,
+    @SerialName("total_results") val totalResults: Int
+)
