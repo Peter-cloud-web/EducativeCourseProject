beb36bf0d792737c7772940180c711757663ee2a
Peter-cloud-web <petergichia165@gmail.com>
2024-02-02 11:40:54 +0300

    Remove app module

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_educativecourseproject_presentation_MovieApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.educativecourseproject.presentation.MovieApp"
-)
-public final class _com_example_educativecourseproject_presentation_MovieApp {
-}
