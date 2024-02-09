0043b8e3c34f0941698f9321766f299d0ed5bd93
Peter-cloud-web <petergichia165@gmail.com>
2024-02-09 19:13:17 +0300

    Add a feature to serach movies

--- presentation/src/main/java/com/example/educativecourseproject/presentation/ui/fragments/SearchMovieFragment.kt
+++ presentation/src/main/java/com/example/educativecourseproject/presentation/ui/fragments/SearchMovieFragment.kt
@@ -1,60 +1,109 @@
 package com.example.educativecourseproject.presentation.ui.fragments
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import coil.ImageLoader
+import com.example.cinemaxv3.util.Constants
+import com.example.cinemaxv3.view.ui.adapter.SearchMoviesAdapter
+import com.example.cinemaxv3.viewmodels.searchedMoviesViewModel.SearchedMoviesViewModel
 import com.example.educativecourseproject.R
+import com.example.educativecourseproject.data.mappers.Mappers.toPopularMovie
+import com.example.educativecourseproject.databinding.FragmentSearchMovieBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class SearchMovieFragment : Fragment(R.layout.fragment_search_movie) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchMovieFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SearchMovieFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var searchMoviesAdapter: SearchMoviesAdapter
+    private val searchedMoviesViewModel: SearchedMoviesViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentSearchMovieBinding.bind(view)
+        val imageLoader = ImageLoader(requireContext())
+
+        searchMoviesAdapter = SearchMoviesAdapter(imageLoader)
+
+        handleActioBar()
+        searchMovies(binding)
+        submitMovies(binding)
+        handleOnClick()
+    }
+
+    private fun handleActioBar(){
+        val actionbar = (activity as AppCompatActivity).supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setBackgroundDrawable(context?.let {
+                ContextCompat.getColor(
+                    it,
+                    R.color.black
+                )
+            }?.let { ColorDrawable(it) })
+            title = "Search"
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_movie, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchMovieFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchMovieFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun searchMovies(binding: FragmentSearchMovieBinding){
+        var job: Job? = null
+        binding.searchView.addTextChangedListener { searchEditText ->
+            job?.cancel()
+            job = MainScope().launch() {
+                delay(Constants.SEARCH_NEWS_TIME_DELAY)
+                searchEditText?.let { editText ->
+                    if (editText.toString().isNotEmpty()) {
+                        searchedMoviesViewModel.searchMovies(editText.toString())
+                    }
                 }
             }
+        }
     }
+
+    private fun submitMovies(binding: FragmentSearchMovieBinding){
+        searchedMoviesViewModel.SearchResponse.observe(viewLifecycleOwner, Observer { response ->
+            searchMoviesAdapter.searchMovies.submitList(response?.results?.map { movie->
+                movie.toPopularMovie()
+            })
+        })
+
+        binding.searchRecyclerView.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = searchMoviesAdapter
+        }
+    }
+
+    private fun handleOnClick(){
+        searchMoviesAdapter.setOnItemClickListener { movie ->
+
+            with(movie) {
+
+                val action = SearchMovieFragmentDirections.actionSearchMovieFragment2ToMovieDetailsFragment(
+                    Constants.IMAGE_BASE_URL + poster_path,
+                    Constants.IMAGE_BASE_URL + backdrop_path,
+                    title.toString(),
+                    overview.toString(),
+                    vote_average.toFloat(),
+                    id
+                )
+                findNavController().navigate(action)
+
+            }
+        }
+    }
+
 }
