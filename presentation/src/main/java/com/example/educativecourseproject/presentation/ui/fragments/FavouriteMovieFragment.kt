487a69865e302030701c8458920d32d8c495e39c
Peter-cloud-web <petergichia165@gmail.com>
2024-02-11 22:49:40 +0300

    switch threads with dispatchers

--- presentation/src/main/java/com/example/educativecourseproject/presentation/ui/fragments/FavouriteMovieFragment.kt
+++ presentation/src/main/java/com/example/educativecourseproject/presentation/ui/fragments/FavouriteMovieFragment.kt
@@ -1,11 +1,9 @@
 package com.example.educativecourseproject.presentation.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -18,8 +16,11 @@ import com.example.domain.entities.model.favourites.FavouriteMovies
 import com.example.educativecourseproject.R
 import com.example.educativecourseproject.databinding.FragmentFavouriteMovieBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 @AndroidEntryPoint
 class FavouriteMovieFragment : Fragment(R.layout.fragment_favourite_movie) {
     private lateinit var favouriteMoviesAdapter: FavouriteMoviesAdapter
@@ -65,28 +66,33 @@ class FavouriteMovieFragment : Fragment(R.layout.fragment_favourite_movie) {
     }
 
     private fun populateRecyclerView(binding: FragmentFavouriteMovieBinding) {
-        viewLifecycleOwner.lifecycleScope.launch {
-            favouriteMoviesViewModel.favouriteMovies.collectLatest { uiStates ->
 
-                with(uiStates) {
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
 
-                    when {
-                        isLoading -> {}
+            withContext(Dispatchers.Main) {
 
-                        favouriteMovies != null -> {
-                            uiStates.favouriteMovies.collect { favouriteMoviesList ->
-                                favouriteMoviesAdapter.favouriteMovies.submitList(
-                                    favouriteMoviesList
-                                )
+                favouriteMoviesViewModel.favouriteMovies.collectLatest { uiStates ->
+
+                    with(uiStates) {
+
+                        when {
+                            isLoading -> {}
+
+                            favouriteMovies != null -> {
+                                uiStates.favouriteMovies.collect { favouriteMoviesList ->
+                                    favouriteMoviesAdapter.favouriteMovies.submitList(
+                                        favouriteMoviesList
+                                    )
+                                }
                             }
-                        }
 
-                        error != null -> {
-                            Toast.makeText(
-                                requireContext(),
-                                "An unexpected error occurred",
-                                Toast.LENGTH_SHORT
-                            ).show()
+                            error != null -> {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "An unexpected error occurred",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         }
                     }
                 }
